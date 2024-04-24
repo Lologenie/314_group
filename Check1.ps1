6ac9275ccb40e89bb6f4f4cffed2bc40cc45c379
Lologenie <romandidkovskij@gmail.com>
2024-04-24 11:44:56 +0300

    deleted files

--- Check1.ps1
+++
@@ -1,5 +0,0 @@
-﻿$runningServices = Get-Service | Where-Object { $_.Status -eq "Running" }
-$stoppedServices = Get-Service | Where-Object { $_.Status -eq "Stopped" }
-
-Write-Host "Кількість працюючих служб: $($runningServices.Count)"
-Write-Host "Кількість зупинених служб: $($stoppedServices.Count)"
